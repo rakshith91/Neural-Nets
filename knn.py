822549430b55f51d0300f4081ebc694e4e9ba7d4
jeffravi <jeffravi@iu.edu>
2016-12-13 21:40:49 -0500

    Added Description for KNN

--- knn.py
+++ knn.py
@@ -2,6 +2,57 @@ import collections
 import numpy as np
 import time
 
+'''
+
+Parameters
+K:              Indicates the number of nearest neighbours to be considered. Can be changed at Runtime
+                    1. k should be odd
+                    2. should not be the multiple of number of classes
+feature_count:  Its hard coded in the program but can be adjusted by changing the "matrix" variable in filter() in knn.py
+Accuracy:       Accuracy of the classifier, comparing with the predicted and actual values
+
+K   feature_count(8x8 = 64) Accuracy    Time(On Local Machine)
+200        20               71.36       680 sec
+100        20               71.89       680 sec
+101        12               70.6        442 sec
+53         20               70.51       650 sec
+
+
+How code Works:
+For each test_data
+1. Euclidean distance of the test_data from every training data is calculated
+2. Then the K nearest neighbours with respect to the distance is considered
+3. Among the K nearest neighbours labels having higher count will be the predicted label of the test data
+
+Problems faced
+Choosing the correct features -- solution is using a filter as mentioned below
+
+Simplification --Intuition of choosing edges in the images
+Filter:
+This filter function considers only the pixels configured in the matrix variable
+0 degree - images will have lighter pixels at the top and darker pixels at bottom this implies
+90 degree - image is rotated lighter pixels at the right and darker pixed in the left
+180 degree - image is rotated lighter pixels at the bottom and darker pixed in the top
+270 degree - image is rotated lighter pixels at the left and darker pixed in the right
+
+
+'''
+def filter():
+    max_col = 192
+    tup_size = 3
+    matrix = [[1, 1, 1, 0, 0, 1, 1, 1], [1, 0, 0, 0, 0, 0, 0, 1], [1, 0, 0, 0, 0, 0, 0, 1], [0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0], [1, 0, 0, 0, 0, 0, 0, 1], [1, 0, 0, 0, 0, 0, 0, 1], [1, 1, 1, 0, 0, 1, 1, 1]]
+    # matrix = [[1, 1, 0, 0, 0, 0, 1, 1], [1, 0, 0, 0, 0, 0, 0, 1], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0],
+    #           [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [1, 0, 0, 0, 0, 0, 0, 1], [1, 1, 0, 0, 0,0, 1, 1]]
+    f = np.array(matrix).ravel()
+    feature_idx = []
+    for i in range(0, max_col):
+        idx = (i + tup_size) / tup_size
+        if (f[idx - 1] == 1):
+            # print str(idx) +":: "+ str(i)
+            feature_idx.append(i)
+    return feature_idx
+
 class Data(object):
     def __init__(self,id,label,features):
         self.id = id
@@ -34,20 +85,6 @@ def parse_rawdata(filename):
         data_lst.append(Data(chunk[0],chunk[1],map(int, chunk[2:])))
     return data_lst
 
-def filter():
-    max_col = 192
-    tup_size = 3
-    matrix = [[1, 1, 1, 0, 0, 1, 1, 1], [1, 0, 0, 0, 0, 0, 0, 1], [1, 0, 0, 0, 0, 0, 0, 1], [0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0], [1, 0, 0, 0, 0, 0, 0, 1], [1, 0, 0, 0, 0, 0, 0, 1], [1, 1, 1, 0, 0, 1, 1, 1]]
-    f = np.array(matrix).ravel()
-    feature_idx = []
-    for i in range(0, max_col):
-        idx = (i + tup_size) / tup_size
-        if (f[idx - 1] == 1):
-            # print str(idx) +":: "+ str(i)
-            feature_idx.append(i)
-    return feature_idx
-
 #Calculates the euclidean distance between a single test data and  the list of testing data
 def euclidean_distance(test_data,train_lst):
     print "IN:: euclidean_distance"
@@ -56,10 +93,8 @@ def euclidean_distance(test_data,train_lst):
     for train_data in train_lst:
         distance_lst = []
         for i in range(len(test_data.features)):
-            # print str((test_data.features[i] - train_data.features[i])**2)
             distance_lst.append((float)(test_data.features[i] - train_data.features[i])**2)
         ed = sum(distance_lst)
-        # print "ed:: " + str(ed)
         if(ed in distance_vector):
             distance_vector[ed].append(train_data)
         else:
@@ -84,7 +119,7 @@ def euclidean_distance_i(test_data,train_lst,feature_lst):
 # What should happen when two vectors are having a same distance
 def nearest_neighbors(test_lst,train_lst,k):
     filter_idx = filter()
-    print filter_idx
+
     for test_data in test_lst:
         distance_vector = euclidean_distance_i(test_data, train_lst,filter_idx)
         # sorted(distance_vector.keys())[:k] - gets the k nearest values from the distance_vector
@@ -103,13 +138,23 @@ def classify(train,test,k):
     test_lst = parse_rawdata(test)
     train_lst = parse_rawdata(train)
     start_time = time.time()
+    print("--- start ---" , (start_time))
     nearest_neighbors(test_lst,train_lst,k)
     i = 0
     correct = 0
+    result = []
+    f = open("knn_output.txt", "w")
+    f.close()
+    print "K = "+str(k)
     for test_data in test_lst:
-        print str(i)+" --id:: "+test_data.id+" --a:: "+test_data.label +" --p:: " +test_data.predicted
+        # print str(i)+" --id:: "+test_data.id+" --a:: "+test_data.label +" --p:: " +test_data.predicted
         if(test_data.label == test_data.predicted):
             correct+=1
         i+=1
-    print "correct:: "+str(correct)+"/"+str(i)
+        result.append((test_data.label,test_data.predicted))
+        f = open("knn_output.txt", "a")
+        f.write(test_data.id + " " + str(test_data.predicted) + "\n")
+        f.close()
+    print "Accuracy:: ",float(correct)*100/(i),"%"
     print("--- %s seconds ---" % (time.time() - start_time))
+    return result
--- orient.py
+++ orient.py
@@ -3,59 +3,67 @@ from nnets import *
 import sys
 import adaboost
 
+"""
+Please refer to the following files for more info
+1. adaboost.py
+2. knn.py
+3. nnet.py
+In assignment KNN give better accuracy (71.3 %)but takes 10 min for testing 943 values
+
+"""
+
 def confusionMatrix(cm):
-    #cm = [(actual,pred),(actual,pred),(actual,pred)]
-    lst =   (0,90,180,270)
-    matrix = [[0,0,0,0],\
-              [0,0,0,0],\
-              [0,0,0,0],\
-              [0,0,0,0]]
-
-    for actual,pred in cm:
-        i = lst.index(actual)
-        j = lst.index(pred)
+    print "--confusionMatrix--"
+    # cm = [(actual,pred),(actual,pred),(actual,pred)]
+    lst = (0, 90, 180, 270)
+    matrix = [[0, 0, 0, 0], \
+              [0, 0, 0, 0], \
+              [0, 0, 0, 0], \
+              [0, 0, 0, 0]]
+    print cm
+    for actual, pred in cm:
+        i = lst.index(int(actual))
+        j = lst.index(int(pred))
         matrix[i][j] += 1
-
 
     print "Confusion Matrix is Below: (0,90,180,270)"
     for i in range(len(matrix)):
         for j in range(len(matrix)):
-            print str(matrix[i][j])+" ",
+            print str(matrix[i][j]) + " ",
         print "\n"
 
-def main():
-	print "hiii"
-	print sys.argv
-	technique = sys.argv[3]
-    # (train,test,classifier)=sys.argv[1:4]
-    # (train, test, technique) = ("train-data-mod.txt","test-data-mod.txt","nearest")
-    #(train, test, technique) = ("train-data.txt", "test-data.txt", "nearest")
-    #define the number of neighbours to be considered
-    # 1. k should be odd 2. should not be the multiple of number of classes
-    #k = 200
-    #print (train,test,technique)
-	if technique in "nearest" :
-		'''KNN is simple but complexity is the problem - if number of sample data is huge then the algorithm
-        has a huge complexity'''
-		knn.classify(train,test,k)
-	if(technique== "nnet"):
-		input_data, class_labels, names = read_data(sys.argv[1])
-		cd = {'0' : 0, '90': 1, '180':2, '270':3}
-		weights_one, weights_two= train(input_data,class_labels, int(sys.argv[-1]))
-		output_data, oclass_labels, test_names = read_data(sys.argv[2])
-		pred = feed_forward(input_data, weights_one, weights_two)
-		print "Train Accuracy is" ,accuracy(pred, class_labels)
-		test_pred = feed_forward(output_data, weights_one, weights_two)
-		
-		print "Test Accuracy is" ,accuracy(test_pred, oclass_labels)
-		cf_list = write_to_file(test_pred,oclass_labels, test_names)
-		confusionMatrix(cf_list)
 
-		print cf_list[:10]
-		#print time.time()-start
-	if technique == "adaboost":
-		"python orient.py train_file.txt test_file.txt adaboost stump_count"
-		print sys.argv[0],sys.argv[1],sys.argv[2],sys.argv[4]
-		adaboost.main(sys.argv[1],sys.argv[2],int(sys.argv[4]))
+def main():
+    # (train,test,classifier)=sys.argv[1:4]
+    (train, test, technique) = ("train-data-mod.txt", "test-data-mod.txt", "nearest")
+    # print (train,test,technique)
+
+    if (technique in ("nearest","best")):
+        k = 101
+        # (train, test, technique) = ("train-data-mod.txt", "test-data-mod.txt", "nearest")
+        '''KNN is simple but complexity is the problem - if number of sample data is huge then the algorithm
+        has a huge complexity'''
+        result = knn.classify(train, test, k)
+        confusionMatrix(result)
+    if (technique == "nnet"):
+        input_data, class_labels, names = read_data(sys.argv[1])
+        cd = {'0': 0, '90': 1, '180': 2, '270': 3}
+        weights_one, weights_two = train(input_data, class_labels, int(sys.argv[-1]))
+        output_data, oclass_labels, test_names = read_data(sys.argv[2])
+        pred = feed_forward(input_data, weights_one, weights_two)
+        print "Train Accuracy is", accuracy(pred, class_labels)
+        test_pred = feed_forward(output_data, weights_one, weights_two)
+
+        print "Test Accuracy is", accuracy(test_pred, oclass_labels)
+        cf_list = write_to_file(test_pred, oclass_labels, test_names)
+        confusionMatrix(cf_list)
+
+        print cf_list[:10]
+    # print time.time()-start
+    if technique == "adaboost":
+        "python orient.py train_file.txt test_file.txt adaboost stump_count"
+        print sys.argv[0], sys.argv[1], sys.argv[2], sys.argv[4]
+        adaboost.main(sys.argv[1], sys.argv[2], int(sys.argv[4]))
+
 
 main()
